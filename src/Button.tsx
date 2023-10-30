6e3f4dccc72ec37be5b18b7ff95d0fbea5dc2442
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2023-10-30 05:08:14 +0100

    update

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,13 +3,12 @@ import React from 'react'
 type ButtonProps = {
     text: string
     className?: string
-    // onClick: () => void
+
 } & React.ButtonHTMLAttributes<HTMLButtonElement>
 
-export const Button = ({ text/*, onClick*/, className, ...props }: ButtonProps) => {
+export const Button = ({ text, className }: ButtonProps) => {
     return (
         <>
-            {/* <button {...props} >{text}</button> */}
             <p className={className}>Hello {text} !</p>
         </>
     )
