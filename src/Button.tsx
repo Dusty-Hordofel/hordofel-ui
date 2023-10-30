b2e8ffa130c9ca4b694d4b416d8e8310807c9851
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2023-10-30 05:28:17 +0100

    update

--- src/Button.tsx
+++ src/Button.tsx
@@ -7,9 +7,18 @@ type ButtonProps = {
 
 } & React.ButtonHTMLAttributes<HTMLButtonElement>
 
+
+// const buttonStyle = {
+//     left: "50%",
+//     position: "fixed",
+//     bottom: "50%",
+//     transform: "translate(-50%, -50%)",
+//     zIndex: 1,
+// };
+
 export const Button = ({ text, className }: ButtonProps) => {
     return (
-        <div className='button-position'>
+        <div style={{ left: "50%", position: "fixed", bottom: "50%", transform: "translate(-50%, -50%)", zIndex: 1, }}>
             <p className={className}>Hello {text} !</p>
         </div>
     )
