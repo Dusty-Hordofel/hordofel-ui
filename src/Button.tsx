d23f0747cfc171fa8b806c5d34d07c02efe18f79
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2023-10-31 22:11:48 +0100

    Update Button.tsx

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 type ButtonProps = {
     text: string
     className?: string
+    children?: React.ReactNode
 
 } & React.ButtonHTMLAttributes<HTMLButtonElement>
 
@@ -15,10 +16,22 @@ type ButtonProps = {
 //     zIndex: 1,
 // };
 
-export const Button = ({ text, className }: ButtonProps) => {
+const modalStyle = {
+    left: "50%",
+    position: "fixed" as "fixed", ,
+    bottom: "50%",
+    transform: "translate(-50%, -50%)",
+    zIndex: 1,
+};
+
+export const Button = ({ text, className, children }: ButtonProps) => {
     return (
-        <div style={{ left: "50%", position: "fixed", bottom: "50%", transform: "translate(-50%, -50%)", zIndex: 1, }}>
-            <p className={className}>Hello {text} !</p>
+        // <div style={{ left: "50%", position: "fixed", bottom: "50%", transform: "translate(-50%, -50%)", zIndex: 1, }} className='modal'>
+        <div style={modalStyle} className='modal'>
+            <div className='modal-content'>
+                <p className={className}>Hello {text} !</p>
+                {children}
+            </div>
         </div>
     )
 }
